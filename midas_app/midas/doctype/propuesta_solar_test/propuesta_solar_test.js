59c748615c71a28dd3484ccde492e03f049440f8
Daniel Cruz <danielcruzp@unitec.edu>
2026-08-09 21:31:55 +0000

    feat: inicializar midas_app

---
+++ midas_app/midas/doctype/propuesta_solar_test/propuesta_solar_test.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, MIDAS and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Propuesta Solar Test", {
+// 	refresh(frm) {
+
+// 	},
+// });
