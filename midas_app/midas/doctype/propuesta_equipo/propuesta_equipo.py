fbcec3954de402dd89d15e40951cf124ebfe1874
Daniel Cruz <danielcruzp@unitec.edu>
2026-09-24 00:13:30 -0600

    fix: algunos campos de equipos y cotizacion no se podian sobreescribir
    Los campos de descripcion (cotizacion) y tipo de equipo, modelo, marca, unidad, voltaje (equipos)

--- midas_app/midas/doctype/propuesta_equipo/propuesta_equipo.py
+++ midas_app/midas/doctype/propuesta_equipo/propuesta_equipo.py
@@ -1,15 +1,8 @@
 # Copyright (c) 2026, MIDAS and contributors
 # For license information, please see license.txt
 
-import frappe
 from frappe.model.document import Document
 
 
 class PropuestaEquipo(Document):
 	pass
-
-
-@frappe.whitelist()
-def get_item_details(item_code):
-	equipo = frappe.get_cached_doc("Equipo", item_code)
-	return {"item_name": equipo.nombre}
